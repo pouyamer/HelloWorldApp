0a89ca45ed121127cb574ed21c7c208d68e44fac
Pouya Moazzezi <pmoazzezi@gmail.com>
2025-02-21 19:27:08 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApp {
+  class App {
+    static void Main(string[] args) {
+      Console.BackgroundColor = ConsoleColor.DarkYellow;
+      Console.ForegroundColor = ConsoleColor.Black;
+      Console.SetWindowSize(100, 500);
+      Console.WriteLine("Hello World");
+
+    }
+  }
+}
+
